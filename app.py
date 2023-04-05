df5da8d250ccaf91f4b0c63ec1d680088b6e251c
ebrahim <ebrahimaliyou@gmail.com>
2023-04-05 21:53:12 +0530

    added router /data to fetch and post data

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
-from flask import Flask
+import json
+from flask import Flask, request
 
 app = Flask(__name__)
+
+dataCollection = {
+    "data1": {
+        "name": "sudo",
+        "email": "sudo@sudox.com"
+    }
+}
 
 
 @app.route("/")
@@ -8,5 +16,15 @@ def hello_world():
     return "<h1>sudo user x </h1>"
 
 
+@app.route("/data", methods=['GET', 'POST'])
+def data():
+    # content_type = request.headers.get('Content-Type')
+    if request.method == 'POST':
+        dataCollection["new data"] = request.json
+        return dataCollection
+    else:
+        return dataCollection
+
+
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run(debug=True)
